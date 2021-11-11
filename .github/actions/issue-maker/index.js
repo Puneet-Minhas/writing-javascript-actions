7b54ace9260ef76f2d7f0b43696011a7c99a5e75
Puneet Minhas <puneetminhas@microsoft.com>
2021-11-11 14:27:12 +0000

    create index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -9,13 +9,16 @@ async function run() {
       const token = core.getInput("repo-token");
   
       const octokit = github.getOctokit(token);
-  
+      const repoName = github.context.repo.repo
+
       const newIssue = await octokit.rest.issues.create({
           repo: github.context.repo.repo,
           owner: github.context.repo.owner,
           title: issueTitle,
           body: jokeBody
       });
+      console.log(`Issue created successfully in your repo, ${repoName} `);
+      
     } catch (err) {
         core.setFailed(err.message);
     }
