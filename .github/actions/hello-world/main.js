2abdd11b0a108d8e436080cb643e420ebd770f89
Puneet-Minhas <81772545+Puneet-Minhas@users.noreply.github.com>
2021-12-01 10:23:39 +0000

    Update main.js

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,11 +1,7 @@
 const core = require("@actions/core");
 
-const firstGreeting = core.getInput("first-greeting");
-const secondGreeting = core.getInput("second-greeting");
-const thirdGreeting = core.getInput("third-greeting");
+const greeting = core.getInput("greeting");
 
-console.log(`Hello ${firstGreeting}`);
-console.log(`Hello ${secondGreeting}`);
-if (thirdGreeting) {
-    console.log(`Hello ${thirdGreeting}`);
+console.log(`${greeting}`);
+
 }
